b3861d899edff468ccb184a96950af74f97fd02b
AlexFR0 <127567514+praatts@users.noreply.github.com>
2025-01-28 16:42:21 +0100

    Primer Cas: Èxit
    S'intenta retirar una quantitat menor al balanç del compte, al utilitzar el assertEquals, el valor esperat amb el real coincideix, no llança ninguna excepció.

--- SimpleBankingSystem/AccountTest.java
+++ SimpleBankingSystem/AccountTest.java
@@ -5,10 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class AccountTest {
-
+	 Account myAccount = new Account("Flor Martinez", "1000-1234-56-123456789", 2500);
 	@Test
 	void testWithdrawAmount() {
-		fail("Not yet implemented");
+		try {
+	    	myAccount.withdrawAmount(2300);
+	    } catch(Exception e){
+        	System.err.println(e.getMessage());
+            System.out.println("Error al retirar");
+	    }
+		assertEquals(200, myAccount.getBalance());
+		
 	}
 
 }
