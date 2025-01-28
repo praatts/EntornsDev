bd7cca0ae2f92107226c6fd39ab8c64e2d001e98
AlexFR0 <127567514+praatts@users.noreply.github.com>
2025-01-28 16:34:20 +0100

    Primer Cas: Èxit
    Al ingressar un número positiu, funciona correctament sense llançar cap tipus d'excepció.

--- SimpleBankingSystem/AccountTest.java
+++ SimpleBankingSystem/AccountTest.java
@@ -5,10 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class AccountTest {
+	Account myAccount = new Account("Albert Prats", "1000-1234-56-123456789", 2500);
 
 	@Test
 	void testDepositAmount() {
-		fail("Not yet implemented");
+		try {
+			myAccount.depositAmount(200);
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			System.out.println("Error en l'ingrés");
+		}
+		
+		assertEquals(2700, myAccount.getBalance());
 	}
 
 }
